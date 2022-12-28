12c8efc834f07ce4d331b173c243fc084246d27a
jenhung83 <jenhung83@yahoo.com.tw>
2022-12-28 20:06:25 +0800

    useState v1

--- src/index.js
+++ src/index.js
@@ -1,5 +1,29 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
+import React, { useState } from 'react';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+function App() {
+	const [fname, setFname] = useState('');
+	const getName = (e) => {
+		console.log(e.target.value);
+	};
+	const setName = () => {
+		setFname(fname);
+	};
+
+	return (
+		<div className="container">
+			<h1>Hello {fname}</h1>
+			<form>
+				<input
+					onChange={getName}
+					name="fName"
+					placeholder="First Name"
+					value={fname}
+				/>
+				<input name="lName" placeholder="Last Name" />
+				<button onClick={setName}>Submit</button>
+			</form>
+		</div>
+	);
+}
+
+export default App;
